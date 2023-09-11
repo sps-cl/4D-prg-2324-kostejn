4595e72844e7f7bc8ebf7cc9e633c0209c5d4ff7
filipkostejnn <filip.kostejn3@seznam.cz>
2023-09-11 14:56:52 +0200

    něco

---
+++ Program.cs
@@ -0,0 +1,21 @@
+class Rectangle {
+    private int w;
+    private int h;
+
+    void draw() {
+
+    }
+
+    private void ChangeShape() {
+        Console.WriteLine("měním");
+    }
+}
+
+class program {
+    public static void Main (string[] args) {
+        Rectangle r1 = new Rectangle();
+        r1.Render();
+    }
+}
+
+//ztratil jsem se
