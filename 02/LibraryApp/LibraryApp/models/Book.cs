b008a6f39982c00f08bceffe6d392f1fc77c89c3
filipkostejnn <filip.kostejn3@seznam.cz>
2023-09-18 15:10:35 +0200

    Uložení LibraryApp z hodiny

---
+++ 02/LibraryApp/LibraryApp/models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp.models
+{
+    internal class Book : LibraryItem<Book>
+    {
+        public Book(int NumberOfPages, string Author, int id, string Title) : base(id, Title)
+        {
+            this.NumberOfPages = NumberOfPages;
+            this.Author = Author;
+        }
+
+        public int NumberOfPages { get; }
+        public string Author { get; }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Autor {Author} a počet stran: {NumberOfPages}");
+        }
+    }
+}
---
+++ 02/LibraryApp/LibraryApp/models/DVD.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp.models
+{
+    internal class DVD : LibraryItem<DVD>
+    {
+        public DVD(string Director, int id, string Title) : base(id, Title)
+        {
+            this.Director = Director;
+        }
+
+        public string Director { get; }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Autor {Director}");
+        }
+    }
+    }
+
